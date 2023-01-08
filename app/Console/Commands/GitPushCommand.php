99d2afba6a0b552b5a7e95a45958704fef956153
saylaww <saylaww@gmail.com>
2023-01-08 22:17:44 +0500

    terminal arqali

--- app/Console/Commands/GitPushCommand.php
+++ app/Console/Commands/GitPushCommand.php
@@ -4,6 +4,8 @@ namespace App\Console\Commands;
 
 use Illuminate\Console\Command;
 use Symfony\Component\Process\Process;
+//use Symfony\Component\Process;
+
 
 class GitPushCommand extends Command
 {
@@ -14,13 +16,26 @@ class GitPushCommand extends Command
     public function handle()
     {
         $message = $this->argument("message");
+//        $proc = new Process\Process(['php artisan make:model',$message]);
+//        $proc->run();
+//        $p = Process\Process::fromShellCommandline($proc);
+//        $p->run();
+//new Process\Process()
+//        $message = $this->argument("message");
+//        $proc = new Process\Process(['php artisan make:model',$message]);
+//
+//        $proc->run();
 
+/////
+///
         (new Process(['git','add','.']))->run();
         (new Process(['git','commit','-m', $message]))->run();
         (new Process(['git','push']))->run();
-
+//
         $this->info("Git jan`alandi");
 
 //        return Command::SUCCESS;
     }
+
+
 }
